15418784d58a63ac5b9c1f78b523a658f1f8c974
Viggiecc <vigiecc@gmail.com>
2015-06-28 14:35:02 -0700

    comments page
    This is the comments page , php part

--- index.php
+++ index.php
@@ -1,10 +1,137 @@
+<?php 
+		
+	if($_POST["submit"]){
+	
+		
+		
+		if(!$_POST["name"]){
+		
+			$error="<br/>Please enter your name.";
+			
+		}
+		
+		if(!$_POST["email"]){
+		
+			$error.="<br/>Please enter your email.";
+			
+		}
+		
+		if(!$_POST["comment"]){
+		
+			$error.="<br/>Please enter your comment.";
+			
+		}
+		
+		// if ($_POST["email"]!="" AND !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) { 
+//     		
+//     		$error.="<br/>Please enter a valid email address!"; 
+//     		
+// 		} 
+		
+		if($error){
+		
+			$result='<div class="alert alert-danger"><strong>There exists some error(s):</strong>'.$error.'</div>';
+		
+		}else{
+		
+			// //mail($emailTo, $subject, $body, $headers)
+			if(mail("", "Comment From Website", "Name :" .$_POST['name']."Email: ".$_POST['email']."Comment: ".$_POST['comment'])){
+			
+				$result='<div class="alert alert-success"><strong>Thank!</strong> I\'ll be in touch</div>';
+				
+				
+			}else{
+			
+			
+				$result='<div class="alert alert-danger"><strong>Sorry,there was an error sending your message. Please try again later! <strong></div>';
+			
+			}
+			
+		}
+		
+	}
+
+
+
+?>
+
 <!doctype html>
 <html>
 <head>
-<title>My First Webpage</title> 
+<title>Comments page</title> 
 <meta charset="utf-8" />
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" /> 
 <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap
+.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap
+-theme.min.css">
+
+<style>
+
+.emailForm{
+
+	border: 2px solid white;
+	border-radius: 10px;
+	margin-top: 60px;
+	opacity: 20%;
+	/* 
+background-color: #ea4444;
+	background-opacity:
+ */
+	background-color: transparent;
+
+}
+
+.emailForm h1{
+
+	color: white;
+	opacity: 80%;
+}
+
+.emailForm p{
+
+	color: white;
+	opacity: 80%;
+	
+}
+
+
+
+form{
+
+	padding-bottom: 20px;
+	
+}
+
+
+
+.contentDiv{
+
+	width: 100%;
+    background-size: cover;
+    background-image: url("background3.jpg");
+	/* background-color: transparent; */
+}
+
+/* 
+.marginBottom{
+
+	margin-bottom: 66px;
+
+}
+ */
+
+.whiteFont{
+
+	color:white;
+
+}
+</style>
 </head>
 
 <body>
@@ -13,38 +140,38 @@
 	
 
 	<?php
-// **********************GET variables***********************//
+// **********************GET and POST variables***********************//
 	// print_r($_POST);
 // 	
-	$names=array("viggie","cc","joy");
-	
-	if($_POST["submit"]){
-	
-		if($_POST["name"]){
-		
-			foreach($names as $name){
-			
-				if($_POST["name"]==$name){
-				
-					echo "Hello, I know you! Your name is ".$_POST["name"];
-					
-					$knowyou=1;
-					
-				}
-			
-			}
-			
-			if($knowyou!=1){
-			
-				echo "Sorry,".$_POST["name"]."! I don't know you";
-				
-			}
-		
-		 	
-		}
-		
-
-	}
+	// $names=array("viggie","cc","joy");
+// 	
+// 	if($_POST["submit"]){
+// 	
+// 		if($_POST["name"]){
+// 		
+// 			foreach($names as $name){
+// 			
+// 				if($_POST["name"]==$name){
+// 				
+// 					echo "Hello, I know you! Your name is ".$_POST["name"];
+// 					
+// 					$knowyou=1;
+// 					
+// 				}
+// 			
+// 			}
+// 			
+// 			if($knowyou!=1){
+// 			
+// 				echo "Sorry,".$_POST["name"]."! I don't know you";
+// 				
+// 			}
+// 		
+// 		 	
+// 		}
+// 		
+// 
+// 	}
 
 	// if($_GET["submit"]){
 // 
@@ -221,7 +348,8 @@
 
 	?>
 	
-	<form method="post">
+<!-- 
+<form method="post">
 	
 		
 		<label for="name">Name</label>
@@ -230,12 +358,81 @@
 		
 		<input type="submit" name="submit" value="Submit your name"/ >
 		
-		<!-- <button> submit </button> -->
+		<!~~ <button> submit </button> ~~>
 	
 	
 	</form>
+ -->
+ 
+<div class="contentDiv">
+<div class="container " >
 
+		<div class="row ">
+	
+			<div class="col-md-6 col-md-offset-3 emailForm ">
+	
+				<h1 >Your Comments</h1>
+			
+				<?php echo $result; ?>
+			
+				<p class="lead"> Please get in touch- I'll get back to you as soon as I can.</p>
+			
+				<form method="post" >
+			
+					<div class="form-group">
+			
+						<label for="name" class="whiteFont"> Your Name: </label>
+				
+						<input type="text" name="name" class="form-control" placeholder="Your Name" value="<?php echo $_POST['name'];?>"/ >
+				
+					</div>
+				
+					<div class="form-group">
+			
+						<label for="email" class="whiteFont"> Your Email: </label>
+				
+						<input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $_POST['email'];?>"/ >
+				
+					</div>
+				
+					<div class="form-group">
+			
+						<label for="comment" class="whiteFont"> Your Comment: </label>
+				
+						<textarea class="form-control" name="comment" ><?php echo $_POST['comment']; ?></textarea>
+				
+					</div>
+				
+					<input type="submit" name="submit" value="Submit" class="btn btn-success btn-lg" />
+				
+				</form>
+			
+			</div>
+
+		
+		</div>
+		
+	
+	
 </div>
+</div>
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+
+<script>
+
+        $(".contentDiv").css("height", $(window).height());
+
+       
+</script>
+
+
 </body>
 </html>
 
